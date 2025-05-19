a920688203a702cf9190a528ff2aae348209a92a
zhiqiang <40115555+chihqiang@users.noreply.github.com>
2025-05-19 15:15:31 +0800

    feat: sh

---
+++ release-page.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+# 版本校验
+today_commit=$(curl --silent "https://api.github.com/repos/drawdb-io/drawdb/commits" | jq '.[0].sha' -r)
+current_commit=$(cat currentcommit)
+
+echo "current_commit:$current_commit commit:$today_commit"
+# 判断版本号是否相同 如果相同就exit
+if [[ "$current_commit" == "$today_commit" ]]; then
+    exit
+fi
+
+echo "System initialization"
+git config --global user.name "github-actions[bot]"
+git config --global user.email  "41898282+github-actions[bot]@users.noreply.github.com"
+echo machine github.com login $GITHUB_USER password $GITHUB_TOKEN > ~/.netrc
+
+echo "Updating code repository in progress..."
+echo "Package and publish nodes"
+git clone https://github.com/drawdb-io/drawdb.git code
+cd code
+npm ci
+npm run build
+
+cd dist
+rm -rf .git
+git init
+echo 'gh.drawdb.vhosts.top' > CNAME
+git add -A
+git commit -m "deploy commit $today_commit"
+git push -u origin gh-page --force
+
+cd ..
+cd ..
+
+echo "main branch Version update"
+echo "$today_commit" >currentcommit
+git add currentcommit
+git commit -a -m "Automatically update version to  $today_commit"
+git push -f https://github.com/myhosts/drawdb-io.git main
